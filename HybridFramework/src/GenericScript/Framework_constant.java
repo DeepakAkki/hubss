444bf0f64ef9620d55ebf1d636c6ee334641b75a
Deepak Akki <Deepak Akki@DESKTOP-U3LRL5Q>
2023-07-05 17:01:26 +0530

    build01

---
+++ HybridFramework/src/GenericScript/Framework_constant.java
@@ -0,0 +1,14 @@
+package GenericScript;
+
+public interface Framework_constant 
+{
+String chrome_key="webdriver.chrome.driver";
+String chrome_value="./Softwares/chromedriver.exe";
+
+String gecko_key="webdriver.gecko.driver";
+String gecko_value="./Softwares/geckodriver.exe";
+String baseURL="http://www.facebook.com";
+}
+
+
+
